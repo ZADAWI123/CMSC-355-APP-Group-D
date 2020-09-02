1dd079da2d7dceac6d29479c64a471bdb695b7e0
adawizi <adawizi@vcu.edu>
2020-09-02 14:02:44 -0400

    Added Android Studio project

---
+++ app/src/main/java/com/example/cmsc355app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.cmsc355app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button nextBtn = (Button) findViewById(R.id.nextBtn);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent (getApplicationContext(), MainActivity2.class);
+                startActivity(startIntent);
+            }
+        });
+    }
+}
