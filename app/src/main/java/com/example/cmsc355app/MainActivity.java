d4cec96ee202e1c82691c7ae5f7ce066b1d9711f
adawizi <adawizi@vcu.edu>
2020-09-06 15:26:28 -0400

    Deleted second activity
    The second activity needed to be empty, not basic, so it was removed. Also, some extra files were removed.

--- app/src/main/java/com/example/cmsc355app/MainActivity.java
+++ app/src/main/java/com/example/cmsc355app/MainActivity.java
@@ -2,12 +2,7 @@ package com.example.cmsc355app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button nextBtn = (Button) findViewById(R.id.nextBtn);
-        nextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent (getApplicationContext(), MainActivity2.class);
-                startActivity(startIntent);
-            }
-        });
     }
-/*
+    /*
     public void test(){
 
         final SharedPreferences myPrefs = getSharedPreferences("tag", 0);
@@ -49,12 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 temp = Integer.toString(count[0]);
                 textView2.setText(temp);
             }
-
-
         });
-
-
-
     }
 
  */
