40d0d7add0f78339ea32c37a98a4c4ad7b8b79f1
adawizi <adawizi@vcu.edu>
2020-09-06 17:18:35 -0400

    Added formula for calculating xp and leveling up
    Added a working formula in a comment (for later use) that allows the user to level up and earn points for completing workouts.

--- app/src/main/java/com/example/cmsc355app/MainActivity.java
+++ app/src/main/java/com/example/cmsc355app/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.cmsc355app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,16 +16,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     /*
+    String temp = "";
+    String temp2 = "";
+
+
     public void test(){
+
 
         final SharedPreferences myPrefs = getSharedPreferences("tag", 0);
         final int []count = {myPrefs.getInt("num",0)};
+        final int []currentLevel = {myPrefs.getInt("num2",0)};
+        final int [] points = {myPrefs.getInt("num3",1)};;
+
         //final String[] temp = {""};
 
         temp  = Integer.toString(count[0]);
         setContentView(R.layout.activity_main);
         Button btn = (Button) findViewById(R.id.btn1);
-        //myEditor.putInt("num", count[0]+=5).commit();
+        //myEditor.putInt("num", count[0]++).commit();
 
         btn.setOnClickListener(new View.OnClickListener(){
 
@@ -30,13 +42,24 @@ public class MainActivity extends AppCompatActivity {
                 SharedPreferences.Editor myEditor = myPrefs.edit();
 
                 TextView textView2 = (TextView) findViewById(R.id.textView2);
+                TextView textView = (TextView) findViewById(R.id.textView);
                 myEditor.putInt("num", count[0]++).commit();
+                if(count[0] == points[0] * 2){
+                    myEditor.putInt("num2", currentLevel[0]++).commit();
+                    myEditor.putInt("num3", points[0] = count[0]).commit();
+                }
+
 
                 temp = Integer.toString(count[0]);
+                temp2 = Integer.toString(currentLevel[0]);
                 textView2.setText(temp);
+                textView.setText(temp2);
             }
-        });
-    }
 
- */
+
+        });
+
+    }
+    
+     */
 }
