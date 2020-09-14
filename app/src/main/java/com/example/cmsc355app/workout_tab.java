047d70d6f18c99fd9aa11ea39cc2904e52a1af1f
adawizi <adawizi@vcu.edu>
2020-09-14 10:57:50 -0400

    Created workout tab
    Created a workout tab with a button to test how completing workouts works. Also, moved code that determines xp and leveling up to that tab.

---
+++ app/src/main/java/com/example/cmsc355app/workout_tab.java
@@ -0,0 +1,62 @@
+package com.example.cmsc355app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class workout_tab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_tab);
+    }
+
+    /*
+    String temp = "";
+    String temp2 = "";
+
+
+    public void test(){
+
+
+        final SharedPreferences myPrefs = getSharedPreferences("tag", 0);
+        final int []count = {myPrefs.getInt("num",0)};
+        final int []currentLevel = {myPrefs.getInt("num2",0)};
+        final int [] points = {myPrefs.getInt("num3",1)};;
+
+        //final String[] temp = {""};
+
+        temp  = Integer.toString(count[0]);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btn1);
+        //myEditor.putInt("num", count[0]++).commit();
+
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                SharedPreferences.Editor myEditor = myPrefs.edit();
+
+                TextView textView2 = (TextView) findViewById(R.id.textView2);
+                TextView textView = (TextView) findViewById(R.id.textView);
+                myEditor.putInt("num", count[0]++).commit();
+                if(count[0] == points[0] * 2){
+                    myEditor.putInt("num2", currentLevel[0]++).commit();
+                    myEditor.putInt("num3", points[0] = count[0]).commit();
+                }
+
+
+                temp = Integer.toString(count[0]);
+                temp2 = Integer.toString(currentLevel[0]);
+                textView2.setText(temp);
+                textView.setText(temp2);
+            }
+
+
+        });
+
+    }
+
+     */
+}
