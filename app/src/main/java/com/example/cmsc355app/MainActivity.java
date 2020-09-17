ce5ddd6d8711eafbfa1bffa64c19f453fc90d85c
DustinNash40 <dustin.nash40@gmail.com>
2020-09-16 21:35:46 -0400

    Buttons for Activity
    I have made buttons to go to each activity.

--- app/src/main/java/com/example/cmsc355app/MainActivity.java
+++ app/src/main/java/com/example/cmsc355app/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cmsc355app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +11,29 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //This is the name of the button
+    private Button BtnMove;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtnMove = findViewById(R.id.toHub);
+
+        BtnMove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                moveToHubMain();
+            }
+        });
+
     }
 
+    private void moveToHubMain(){
+
+        Intent intent = new Intent(MainActivity.this, main_hub.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/cmsc355app/main_hub.java
+++ app/src/main/java/com/example/cmsc355app/main_hub.java
@@ -6,13 +6,51 @@ package com.example.cmsc355app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class main_hub extends AppCompatActivity {
+
+    private Button BtnWork;
+    private Button BtnPro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_hub);
+
+        //Workout Button
+        BtnWork = findViewById(R.id.workOutBtn);
+
+        BtnWork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                moveToWorkOut();
+            }
+        });
+
+        //Progression Button
+        BtnPro = findViewById(R.id.progressBtn);
+
+        BtnPro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                moveToProgress();
+            }
+        });
+    }
+    private void moveToWorkOut(){
+
+        Intent intent1 = new Intent(main_hub.this, workout_tab.class);
+        startActivity(intent1);
+    }
+    private void moveToProgress(){
+
+        Intent intent2 = new Intent(main_hub.this, Progression_Tab.class);
+        startActivity(intent2);
     }
 }
