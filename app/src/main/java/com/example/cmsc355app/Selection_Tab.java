686e433a76829ba473a06aec4180547c7bb66a4f
Jack Watkins <watkinsjb@mymail.vcu.edu>
2020-09-18 13:03:24 -0400

    Revert "Created buttons"
    This reverts commit c5eed2a41037353ab3239a6a5b2ac9cdd5c32243.

--- app/src/main/java/com/example/cmsc355app/Selection_Tab.java
+++ app/src/main/java/com/example/cmsc355app/Selection_Tab.java
@@ -1,25 +1,7 @@
-/* This is the tab that allows the user to select bodyweight, weight lifting or cardio exercise*/
 package com.example.cmsc355app;
-
-import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Selection_Tab extends AppCompatActivity {
-
-    private Button cBtn;
-    private Button wlBtn;
-    private Button bwBtn;
-
-    protected void onCreate (Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_selection_tab);
-
-        cBtn = findViewById(R.id.cBtn);
-        wlBtn = findViewById(R.id.wlBtn);
-        cBtn = findViewById(R.id.bwBtn);
-    }
 
 }
