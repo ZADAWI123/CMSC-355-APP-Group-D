586d2d2c7ce6ac2d800bfdca251f2f383dcd8012
DustinNash40 <dustin.nash40@gmail.com>
2020-09-18 11:20:37 -0400

    Fixed .XML and Added random quotes
    I fixed a couple of errors in the .xml files and I added the random quotes to main activity

--- app/src/main/java/com/example/cmsc355app/MainActivity.java
+++ app/src/main/java/com/example/cmsc355app/MainActivity.java
@@ -3,22 +3,43 @@ package com.example.cmsc355app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     //This is the name of the button
     private Button BtnMove;
 
+    TextView stringTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        stringTextView = (TextView)findViewById(R.id.randomQuotes);
+
+        List<String> list = new ArrayList<>();
+
+        list.add("You are a Machine");
+        list.add("You can do this!");
+        list.add("It all starts with you.");
+        list.add("Your limitation—it's only your imagination.");
+        list.add("Sometimes later becomes never.");
+
+        MainActivity obj = new MainActivity();
+
+        stringTextView.setText(stringTextView.getText() + getRandomQuote(list));
+
+
+        //Below is the code for the button
         BtnMove = findViewById(R.id.toHub);
 
         BtnMove.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +52,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //This the random code
+    public String getRandomQuote(List<String> list)
+    {
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
+    }
+
+    //This is the button code
     private void moveToHubMain(){
 
         Intent intent = new Intent(MainActivity.this, main_hub.class);
