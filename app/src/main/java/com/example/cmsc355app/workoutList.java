b9e45ebfe57049a543b189920a0c2d1dde774d9f
brentcredo <46752902+brentcredo@users.noreply.github.com>
2020-10-14 13:28:08 -0400

    moved files

---
+++ app/src/main/java/com/example/cmsc355app/workoutList.java
@@ -0,0 +1,18 @@
+package com.example.cmsc355app;
+
+import java.util.ArrayList;
+import java.util.List;
+class mvm{
+
+    enum back {
+        pullups, rows, deadlifts, barbellrow, austrialianpullup;
+    }
+
+    enum chest {
+        Pushup, Benchpress, inclinebenchpress, declinebenchpress, chestflies;
+    }
+
+    enum leg {
+        Jump, Squats, Lounges, HeavySquats;
+    }
+}
