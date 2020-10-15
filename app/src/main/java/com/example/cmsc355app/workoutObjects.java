560004e5f89c9e39221c6a9ebf572ffc21d4e8b4
brentcredo <46752902+brentcredo@users.noreply.github.com>
2020-10-14 20:27:27 -0400

    list of workouts that can be included in the app

--- app/src/main/java/com/example/cmsc355app/workoutObjects.java
+++ app/src/main/java/com/example/cmsc355app/workoutObjects.java
@@ -12,110 +12,56 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-public class workoutObjects extends AppCompatActivity implements View.OnClickListener {
-
-//    String workout1, workout2, workout3, workout4;
+public class workoutObjects {
 //
-//    public workoutObjects(String workout1, String workout2, String workout3, String workout4){
-//        this.workout1 = workout1;
-//        this.workout2 = workout2;
-//        this.workout2 = workout2;
-//        this.workout2 = workout2;
-//    }
+//                        //weighted workouts
+//                        //chest
+//                        weightList.add("flat bench press");
+//                        weightList.add("incline bench press");
+//                        weightList.add("decline bench press");
+//                        weightList.add("incline dumbbell press");
+//                        weightList.add("decline dumbbell press");
+//                        weightList.add("flat dumbbell press");
+//                        weightList.add("cable flies");
+//                        //leg
+//                        weightList.add("leg curls");
+//                        weightList.add("leg extentions");
+//                        weightList.add("jump squats");
+//                        weightList.add("squats");
+//                        weightList.add("weighted lounges");
+//                        weightList.add("leg press");
+//                        //back
+//                        weightList.add("barbell row");
+//                        weightList.add("deadlift");
+//                        weightList.add("lateral pulldown");
+//                        weightList.add("dumbbell rows");
 //
-//    public String getWorkout1(){
-//        return workout1;
-//    }
-//    public String getWorkout2(){
-//        return workout2;
-//    }
-//    public String getWorkout3(){
-//        return workout3;
-//    }
-//    public String getWorkout4(){
-//        return workout4;
-//    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
-    }
-
-    public List<String> getRandomElement(List<String> exStringList, int num) {
-        Random rand = new Random();
-        List<String> newList = new ArrayList<>();
-        for (int i=0; i < num; i++){
-            int randomIndex = rand.nextInt(exStringList.size());
-            newList.add(exStringList.get(randomIndex));
-            exStringList.remove(randomIndex);
-
-        }
-        return newList;
-    }
-    public List<Integer> getRandomNumber(int num){
-        Random randNum = new Random();
-        int n = num;
-        List<Integer> numList = new ArrayList<>(n);
-        for (int i = 1; i <= num; i++){
-            int randomInt = randNum.nextInt(10);
-            numList.add(randomInt + i);
-        }
-        return numList;
-    }
-
-    public void onClick(View view) {
-
-
-        switch (view.getId()) {
-
-            case R.id.wtBtn:
-                TextView exList = (TextView) view.findViewById(R.id.exList);
-                TextView setList = (TextView) view.findViewById(R.id.numOfReps);
-
-                int num = 4;
-                List <String> weightList = new ArrayList<>();
-                ArrayList<String> printList = new ArrayList<String>();
-                ArrayList<Integer> printSet = new ArrayList<>();
-                weightList.add("pull up");
-                weightList.add("barbell row");
-                weightList.add("deadlift");
-                weightList.add("lateral pulldown");
-                weightList.add("bench press");
-                weightList.add("incline dumbbell");
-                weightList.add("flat dumbbell");
-                weightList.add("cable flies");
-
-
-                printList= (ArrayList<String>)getRandomElement(weightList,num);
-                printSet = (ArrayList<Integer>)getRandomNumber(num);
-                exList.setText(Arrays.toString(printList.toArray()));
-                setList.setText(Arrays.toString(printSet.toArray()));
-
-//            ArrayList legWeight = new ArrayList();
-//            legWeight.add("leg curls");
-//            legWeight.add("leg extentions");
-//            legWeight.add("leg press");
-//            legWeight.add("barbell squats");
+//                        //body weight workouts
+//                        //upper body
+//                        bodyweightList.add("push up");
+//                        bodyweightList.add("pull ups")
+//                                bodyweightList.add("austrailian pullups")
+//                                bodyweightList.add("burpees")
+//                                bodyweightList.add("spiderman pushups")
+//                                //leg
+//                                bodyweightList.add("squats")
+//                                bodyweightList.add("jump squats")
+//                                bodyweightList.add("lounges")
+//                                bodyweightList.add("box jumps")
+//                        //abs
+//                                bodyweightList.add("sit ups")
+//                                bodyweightList.add("planks")
+//                                bodyweightList.add("flutter kicks")
+//
+//                        // cardio
+//                                cardioList.add("100 meter sprints")
+//                                cardioList.add("jog for 30 min")
+//                                cardioList.add("jump rope")
+//                                cardioList.add("bicycle")
 //
 //
-//            ArrayList chestWeight = new ArrayList();
-//            chestWeight.add("bench press");
-//            chestWeight.add("incline dumbbell");
-//            chestWeight.add("flat dumbbell");
-//            chestWeight.add("cable flies");
+//
+//
+//
 
-
-
-//            Object[] backWeightArray = backWeight.toArray();
-//            Object[] chestWeightArray = backWeight.toArray();
-//            Object[] legWeightArray = backWeight.toArray();
-
-            break;
-        }
-    }
 }
