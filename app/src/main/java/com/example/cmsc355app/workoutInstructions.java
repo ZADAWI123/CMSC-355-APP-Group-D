f6249adbae6a0c0f1d5aaa68c6f1c9f2bc1157f1
brentcredo <46752902+brentcredo@users.noreply.github.com>
2020-11-07 21:14:47 -0500

    edited photos and set up image xml

---
+++ app/src/main/java/com/example/cmsc355app/workoutInstructions.java
@@ -0,0 +1,15 @@
+package com.example.cmsc355app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class workoutInstructions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
