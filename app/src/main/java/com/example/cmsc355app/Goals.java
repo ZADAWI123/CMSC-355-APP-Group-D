8435a8f85553cc3cf022e9e4f6e8601cab014def
adawizi <adawizi@vcu.edu>
2020-11-11 17:25:53 -0500

    Finished implementing goal setting feature and fixed issue casuing goals to not show up
    Goals can now be saved and show up after restarting app.

--- app/src/main/java/com/example/cmsc355app/Goals.java
+++ app/src/main/java/com/example/cmsc355app/Goals.java
@@ -3,8 +3,10 @@ package com.example.cmsc355app;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +20,7 @@ public class Goals extends AppCompatActivity {
     private TextView goal2;
     private TextView goal3;
     private EditText goalName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,10 +30,19 @@ public class Goals extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(2000);
         animationDrawable.setExitFadeDuration(4000);
         animationDrawable.start();
+
+        final SharedPreferences myPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        final String[] gl1 = {myPrefs.getString("goal1", "")};
+        final String[] gl2 = {myPrefs.getString("goal2", "")};
+        final String[] gl3 = {myPrefs.getString("goal3", "")};
+        final SharedPreferences.Editor myEditor = myPrefs.edit();
+
+
+
         goalNum = (EditText)findViewById(R.id.goalNum);
-        goal1 = findViewById(R.id.goal1);
-        goal2 = findViewById(R.id.goal2);
-        goal3 = findViewById(R.id.goal3);
+        goal1 = (TextView)findViewById(R.id.goal1);
+        goal2 = (TextView)findViewById(R.id.goal2);
+        goal3 = (TextView)findViewById(R.id.goal3);
         goalName = (EditText)findViewById(R.id.goalName);
 
         setG = findViewById(R.id.setG);
@@ -38,16 +50,21 @@ public class Goals extends AppCompatActivity {
         setG.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(goalNum.getText().equals("1")){
+                if(goalNum.getText().toString().equals("1")){
+                    myEditor.putString("goal1",gl1[0] = goalName.getText().toString()).apply();
                     goal1.setText(goalName.getText());
-                }else if(goalNum.getText().equals("2")){
+                }else if(goalNum.getText().toString().equals("2")){
+                    myEditor.putString("goal2",gl2[0] = goalName.getText().toString()).apply();
                     goal2.setText(goalName.getText());
-                }else if(goalNum.getText().equals("3")){
+                }else if(goalNum.getText().toString().equals("3")){
+                    myEditor.putString("goal3",gl3[0] = goalName.getText().toString()).apply();
                     goal3.setText(goalName.getText());
-                }else{
-
                 }
+                System.out.println( goalNum.getText());
             }
         });
+        goal1.setText(PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString("goal1",""));
+        goal2.setText(PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString("goal2",""));
+        goal3.setText(PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString("goal3",""));
     }
 }
