6620187dab3eb03e4f761208ec9c24ebb0fff849
adawizi <adawizi@vcu.edu>
2020-11-27 11:32:01 -0500

    Created gym finder tab and adjusted layout
    Created gym finder tab that is reachable through the help tab. Also, adjusted the button and text view layouts of the help tab, main hub, and challenge section to create a better look.

--- app/src/main/java/com/example/cmsc355app/gymFinder.java
+++ app/src/main/java/com/example/cmsc355app/gymFinder.java
@@ -6,36 +6,19 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class help extends AppCompatActivity {
-    private Button findGym;
+public class gymFinder extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help);
+        setContentView(R.layout.activity_gym_finder);
+
         ConstraintLayout constraintLayout = findViewById(R.id.layout);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
         animationDrawable.setEnterFadeDuration(3900);
         animationDrawable.setExitFadeDuration(3900);
         animationDrawable.start();
-
-        findGym = findViewById(R.id.findGym);
-
-        findGym.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                moveToGymFinder();
-            }
-        });
-
     }
 
-    private void moveToGymFinder(){
-        Intent intentfg = new Intent(help.this, gymFinder.class);
-        startActivity(intentfg);
-    }
 }
