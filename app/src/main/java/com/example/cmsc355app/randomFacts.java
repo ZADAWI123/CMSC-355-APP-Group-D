5a9b0316161fa1dcb9d97549aba9bfd03021aec3
adawizi <adawizi@vcu.edu>
2020-12-05 12:03:42 -0500

    Fixed fact tab and edited ui elements
    Made fact tab not create new activities every time the button was clicked. Also, fixed string display formatting in fact tab.

--- app/src/main/java/com/example/cmsc355app/randomFacts.java
+++ app/src/main/java/com/example/cmsc355app/randomFacts.java
@@ -23,33 +23,29 @@ public class randomFacts extends AppCompatActivity {
         setContentView(R.layout.activity_random_facts);
         stringTextView = findViewById(R.id.randomText);
 
-        String[] array = {"Health is a state of physical, mental and social well-being in which disease and infirmity are absent.",
-                "The word 'gymnasium' comes from the Greek word gymnazein, which means 'to exercise naked.'",
-                "You burn more calories sleeping than you do watching television.", "During your lifetime, you'll eat about 60,000 pounds of food.",
-                "Laughing 100 times is equivalent to 15 minutes of exercise on a stationary bike.",
-                "Maintaining good relationships with family and friends is good for your health.", "Yoga can boost your cognitive function and lowers stress."};
+        String[] array = {"Health is a state of physical, mental and\nsocial well-being in which disease and infirmity are absent.",
+                "The word 'gymnasium' comes from the Greek word\ngymnazein, which means 'to exercise naked.'",
+                "You burn more calories sleeping\nthan you do watching television.", "During your lifetime, you'll eat\nabout 60,000 pounds of food.",
+                "Laughing 100 times is equivalent to\n15 minutes of exercise on a stationary bike.",
+                "Maintaining good relationships with family\nand friends is good for your health.", "Yoga can boost your cognitive function and lowers stress."};
 
-        List<String> list = Arrays.asList(array);
+        final List<String> list = Arrays.asList(array);
 
         stringTextView.setText(stringTextView.getText() + getRandomFact(list));
 
-        nextFact = (Button)findViewById(R.id.nextBtn);
+        nextFact = (Button) findViewById(R.id.nextBtn);
         nextFact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                moveToNext();
+                stringTextView.setText("");
+                stringTextView.setText(stringTextView.getText() + getRandomFact(list));
             }
         });
     }
-    public String getRandomFact(List<String> list)
-    {
+
+    public String getRandomFact(List<String> list) {
         Random rand = new Random();
         return list.get(rand.nextInt(list.size()));
     }
-
-    private void moveToNext(){
-        Intent intentW = new Intent(randomFacts.this, randomFacts.class);
-        startActivity(intentW);
-    }
 }
+
